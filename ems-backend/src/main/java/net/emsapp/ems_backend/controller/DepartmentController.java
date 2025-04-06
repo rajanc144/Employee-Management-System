3ccc675de39a35f8d1a9b9710281adcbe53537d1
EnzoLeman <ezekielaugustino@gmail.com>
2025-04-06 17:16:48 +0300

    Small Changes..

--- ems-backend/src/main/java/net/emsapp/ems_backend/controller/DepartmentController.java
+++ ems-backend/src/main/java/net/emsapp/ems_backend/controller/DepartmentController.java
@@ -29,7 +29,7 @@ public class DepartmentController {
 
     private DepartmentService departmentService;
 
-    // Build ADD department REST API
+    // Build ADD Department REST API
     @PostMapping()
     public ResponseEntity <DepartmentDto> createDepartment (@RequestBody DepartmentDto departmentDto) {
 
@@ -37,28 +37,28 @@ public class DepartmentController {
         return new ResponseEntity<DepartmentDto>(department, HttpStatus.CREATED);
     }
     
-    // Build GET department REST API
+    // Build GET Department REST API
     @GetMapping("{id}")
     public ResponseEntity<DepartmentDto> getDepartmentById(@PathVariable("id") Long departmentId) {
         DepartmentDto departmentDto = departmentService.getDepartmentById(departmentId);
         return ResponseEntity.ok(departmentDto);
     }
 
-    // Build GET ALL department REST API
+    // Build GET ALL Department REST API
     @GetMapping()
     public ResponseEntity<List<DepartmentDto>> getAllDepartments() {
         List<DepartmentDto> departments = departmentService.getAllDepartments();
         return ResponseEntity.ok(departments);
     }
 
-    // Build UPDATE department REST API
+    // Build UPDATE Department REST API
     @PutMapping("{id}")
     public ResponseEntity<DepartmentDto> updateDepartment(@PathVariable("id") Long departmentId, @RequestBody DepartmentDto updatedDepartment) {
         DepartmentDto departmentDto = departmentService.updateDepartment(departmentId, updatedDepartment);
         return ResponseEntity.ok(departmentDto);
     }
     
-    // Build DELETE department REST API
+    // Build DELETE Department REST API
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteDepartment(@PathVariable("id") Long departmentId) {
     departmentService.deleteDepartment(departmentId);
